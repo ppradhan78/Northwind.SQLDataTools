b713aa0209f93436a239886df1b7068c348c54f1
pradhan prasanta <pradhan.prasanta78@gmail.com>
2022-12-25 16:57:58 +0530

    Sample Sql Server data base project

---
+++ Northwind.SQLDataTools/dbo/Tabels/Categories.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Categories]
+(
+    [CategoryID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CategoryName] NVARCHAR(15) NOT NULL, 
+    [Description] NTEXT NULL, 
+    [Picture] IMAGE NULL
+)
---
+++ Northwind.SQLDataTools/dbo/Tabels/Products.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+    [ProductID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(40) NOT NULL, 
+    [SupplierID] INT NULL, 
+    [CategoryID] INT NULL, 
+    [QuantityPerUnit] NVARCHAR(20) NULL, 
+    [UnitPrice] MONEY NULL, 
+    [UnitsInStock] SMALLINT NULL, 
+    [UnitsOnOrder] SMALLINT NULL, 
+    [ReorderLevel] SMALLINT NULL, 
+    [Discontinued] BIT NOT NULL, 
+    CONSTRAINT [FK_Products_Categories] FOREIGN KEY ([CategoryID]) REFERENCES [Categories]([CategoryID]), 
+    CONSTRAINT [FK_Products_Suppliers] FOREIGN KEY ([SupplierID]) REFERENCES [Suppliers]([SupplierID]) 
+
+)
